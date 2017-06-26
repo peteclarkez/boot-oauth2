49f260ca0b1a09811eb887fd62dc4708cde0ee3c
Peter Clarke <peteclarkez@users.noreply.github.com>
2017-06-26 12:35:08 +0100

    Initial Commit

---
+++ src/main/java/com/clarkez/oauthfesolo/DefaultController.java
@@ -0,0 +1,26 @@
+package com.clarkez.oauthfesolo;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+/**
+ * Created by e23962 on 25/06/2017.
+ */
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String sayHello(Principal principal){
+        String user = "ME";
+        if(principal!=null){
+            user = principal.getName();
+            //String nick = ((Map<String,String>)(((Authentication)principal).getDetails())).get("NickName");
+        }
+
+        return "Hello "+user;
+    }
+}
---
+++ src/test/java/com/clarkez/oauthfesolo/OauthFesoloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clarkez.oauthfesolo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthFesoloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
